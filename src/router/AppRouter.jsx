0ddb811f67e0ccfe46f8ea12c534213179bdefdf
Camilo López <camiloj.lopez79@gmail.com>
2022-08-03 18:55:53 -0400

    Rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 // Pages
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
+import {PrivateRoute} from './PrivateRoute';
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path='login' element={ <LoginPage /> } />
-        <Route path='/*' element={ <HeroesRoutes /> } />
+
+        <Route path='/*' element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        } />
       </Routes>
     </>
   )
