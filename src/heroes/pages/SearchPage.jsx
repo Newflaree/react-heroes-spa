408585eb455033c9763dcab650f1b6ba98eeace1
Camilo López <camiloj.lopez79@gmail.com>
2022-08-03 10:08:28 -0400

    Mostrar mensajes condicionales

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -13,13 +13,16 @@ export const SearchPage = () => {
   const { q = '' } = queryString.parse( search );
   const heroes = getHeroesByName( q );
 
+  const showSearch = ( q.length === 0 );
+  const showError = ( q.length > 0 ) && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
     searchText: q
   });
 
   const onSearchSubmit = ( event ) => {
     event.preventDefault();
-    if ( searchText.trim().length <= 1 ) return;
+    //if ( searchText.trim().length <= 1 ) return;
 
     navigate( `?q=${ searchText.toLowerCase().trim() }` );
   }
@@ -54,13 +57,30 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
+          {
+            /*
+            ( q === '' )
+              ? <div className="alert alert-primary">Search a hero</div>
+              : ( heroes.length === 0 )
+                && <div className="alert alert-danger">There's no heroes with <b>{ q }</b></div>
+              * */
+          }
+
+          <div 
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? '' : 'none' }}
+          >
             Search a hero
           </div>
 
-          <div className="alert alert-danger">
+
+          <div 
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? '' : 'none' }}
+          >
             There's no heroes with <b>{ q }</b>
           </div>
+
 
           {
             heroes.map( hero => (
